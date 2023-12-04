56cb8a64fa73ebbaa4810b7bcfd14d62bf474af1
ahmadjumadi <ahmadjumadi@smkn1karawang.sch.id>
2023-12-04 12:47:37 +0700

    add endpoint /test

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -64,6 +64,14 @@ const createServer = async (container) => {
     }),
   });
 
+  server.route({
+    method: 'GET',
+    path: '/test',
+    handler: () => ({
+      value: 'Test!',
+    }),
+  });
+
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
